d3eba2c1acbc231c580b89e286738ef71036f0ce
tayyipguzel <tguzeldev@gmail.com>
2025-03-26 13:36:58 +0300

    fix: update exit function

--- lib/widgets/security_sheet.dart
+++ lib/widgets/security_sheet.dart
@@ -1,9 +1,8 @@
-import 'dart:ui';
+import 'dart:io';
 
 import 'package:basic_app_security/model/basic_security_delegate.dart';
 import 'package:basic_widgets/basic_widgets.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 
 void showSecuritySheet(
   BuildContext context, {
@@ -17,20 +16,17 @@ void showSecuritySheet(
       width: double.infinity,
       height: 340 + 28,
     ),
-    builder: (context) => BasicSheet(
-      type: BasicSheetType.withOutCloseButton,
-      child: _SecuritySheet(
-        delegate: delegate,
-      ),
-    ),
+    builder:
+        (context) => BasicSheet(
+          type: BasicSheetType.withOutCloseButton,
+          child: _SecuritySheet(delegate: delegate),
+        ),
   );
 }
 
 @immutable
 final class _SecuritySheet extends StatelessWidget {
-  const _SecuritySheet({
-    required this.delegate,
-  });
+  const _SecuritySheet({required this.delegate});
 
   final BasicSecurityDelegate delegate;
 
@@ -47,11 +43,10 @@ final class _SecuritySheet extends StatelessWidget {
                 child: Container(
                   margin: const EdgeInsets.all(24),
                   decoration: BoxDecoration(
-                    borderRadius: const BorderRadius.all(
-                      Radius.circular(24),
-                    ),
-                    color:
-                        Theme.of(context).colorScheme.primary.withOpacity(.12),
+                    borderRadius: const BorderRadius.all(Radius.circular(24)),
+                    color: Theme.of(
+                      context,
+                    ).colorScheme.primary.withOpacity(.12),
                   ),
                   alignment: Alignment.center,
                   child: Column(
@@ -76,14 +71,14 @@ final class _SecuritySheet extends StatelessWidget {
                       Text(
                         delegate.title,
                         style: Theme.of(context).textTheme.titleLarge?.copyWith(
-                              fontWeight: FontWeight.w700,
-                            ),
+                          fontWeight: FontWeight.w700,
+                        ),
                       ),
                       Text(
                         delegate.description,
-                        style: Theme.of(context).textTheme.bodyMedium?.copyWith(
-                              fontSize: 13,
-                            ),
+                        style: Theme.of(
+                          context,
+                        ).textTheme.bodyMedium?.copyWith(fontSize: 13),
                         textAlign: TextAlign.center,
                       ),
                     ],
@@ -91,13 +86,9 @@ final class _SecuritySheet extends StatelessWidget {
                 ),
               ),
               BasicButton.primary(
-                child: Text(
-                  delegate.button,
-                ),
+                child: Text(delegate.button),
                 onPressed: () {
-                  ServicesBinding.instance.exitApplication(
-                    AppExitType.required,
-                  );
+                  exit(0);
                 },
               ),
             ],
